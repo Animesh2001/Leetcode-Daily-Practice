86a03deb7cc02015b0ff251d1e72b5a02a6fe795
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-05-09 16:14:03 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (91.75%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,18 +1,29 @@
 class Solution {
+    
+    //Explanation
+    //1. whenever mid element < high element .. that time our pivot will lie on the left side.. (obviously)
+    //2. whenever mid element > high element .. that time our pivot will lie on the right side.. 
+    //3. at last the lo==hi break and return the pivot element.
+    
+    
     public int findMin(int[] nums) {
         int lo =0;
         int hi =nums.length-1;
         
+        //we will not consider lo==hi ..consider [1].. here lo==hi.. so in this case lo or hi itself is the pivot element.
         while(lo<hi){
             int mid = lo+(hi-lo)/2;
             if(nums[mid]<nums[hi]){
+                //if nums[mid]<nums[hi].. pivot will be on the left
                 hi=mid;
             }
             else{
+                 //if nums[mid]>nums[hi].. pivot will be on the right
                 lo=mid+1;
             }
         }
         
+        //at last the loop will break when lo==hi so return anything num[lo] or nums[hi].. it contains the pivot element.!!
         return nums[hi];
     }
 }
