cd33f34aa7d955186c157e4d4111faf3752d425a
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-05-10 21:55:03 +0530

    Time: 0 ms (100.00%), Space: 48.6 MB (11.16%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
@@ -2,9 +2,7 @@ class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         int start =0;
         int end = letters.length-1;
-        char ans = '#';
-        //if(target=='z')return letters[0];
-        if(letters[0]>target)return letters[0];
+        char ans = letters[0];
         while(start<=end){
             int mid = start+(end-start)/2;
             if(letters[mid]>target){
@@ -14,7 +12,7 @@ class Solution {
                 start=mid+1;
             }
         }
-        return ans=='#'?letters[0]:ans;
+        return ans;
         
     }
 }
