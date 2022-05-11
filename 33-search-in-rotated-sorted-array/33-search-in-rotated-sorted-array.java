512aa53ea1a14e08c0758267ca4dbb66a6a86194
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-05-11 11:21:56 +0530

    Time: 1 ms (61.28%), Space: 42.2 MB (71.35%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -9,25 +9,32 @@ class Solution {
         return ans1==-1?ans2:ans1;
     }
     
-    public int findmin(int nums[]){
+     public int findmin(int nums[]){
         int start = 0;
         int end = nums.length-1;
+       
+       if(nums.length==1){
+           return nums[0];
+       }
+       
+       int ans=-1;
+       int min=Integer.MAX_VALUE;
         
         while(start<=end){
             int mid = start+(end-start)/2;
-            int next = (mid+1)%nums.length;
-            int prev = (mid+nums.length-1)%nums.length;
-            if(nums[mid]<nums[next]&&nums[mid]<nums[prev]){
-                return mid;
+            if(min>nums[mid]){
+                ans=mid;
+                min=nums[mid];
             }
-            else if(nums[mid]>nums[end]){
+            
+            if(nums[mid]>nums[end]){
                 start=mid+1;
             }
             else{
                 end=mid-1;
             }
         }
-        return -1;
+        return ans;
     }
     
     public int search(int nums[], int target, int start, int end){
