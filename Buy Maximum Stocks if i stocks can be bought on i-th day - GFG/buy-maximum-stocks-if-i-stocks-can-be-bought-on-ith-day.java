826b49f826a6d400086e8c6fbab20f97cff0a192
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-05-17 22:53:24 +0530

    Added solution - LeetHub

---
+++ Buy Maximum Stocks if i stocks can be bought on i-th day - GFG/buy-maximum-stocks-if-i-stocks-can-be-bought-on-ith-day.java
@@ -0,0 +1,75 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            String str=br.readLine();
+    		String[] starr=str.split(" ");
+    		
+    		//input n and d
+    	    int n=Integer.parseInt(starr[0]);
+    		int k= Integer.parseInt(starr[1]);
+            
+            starr = br.readLine().trim().split(" ");
+            int[] price = new int[n];
+            for(int i = 0; i < n; i++)
+                price[i] = Integer.parseInt(starr[i]);
+            
+            Solution obj = new Solution();
+            int res = obj.buyMaximumProducts(n, k, price);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    
+    public static class Pair implements Comparable<Pair>{
+        int price,day;
+        public Pair(int price,int day){
+            this.price=price;
+            this.day=day;
+        }
+        public int compareTo(Pair p){
+            return this.price-p.price;
+        } 
+    }
+    
+    public static int buyMaximumProducts(int n, int k, int[] price) {
+        // code here
+        Pair pair[]=new Pair[n];
+        for(int i=0;i<n;i++){
+            Pair p = new Pair(price[i],i+1);
+            pair[i]=p;
+        }
+        Arrays.sort(pair);
+        int count=0;
+        
+        for(int i=0;i<pair.length&&k>0;i++){
+            int amount =pair[i].price*pair[i].day;
+            if(amount<=k){
+                k=k-amount;
+                count+=pair[i].day;
+            }
+            else{
+                while(k-pair[i].price>=0){
+                    k-=pair[i].price;
+                    count++;
+                }
+                break;
+            }
+        }
+        return count;
+        
+    }
+}
+        
