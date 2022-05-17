510d6e67cad7f808ada4c1647309681161e855a1
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-05-17 12:56:50 +0530

    Added solution - LeetHub

---
+++ Job Sequencing Problem - GFG/job-sequencing-problem.java
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
+            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+        // Your code here
+        Arrays.sort(arr,(o1,o2)->{
+           return o2.profit-o1.profit; 
+        });
+        
+        int maxi = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            maxi=Math.max(maxi,arr[i].deadline);
+        }
+        
+        int result[]=new int[maxi+1];
+        
+        for(int i=0;i<=maxi;i++){
+            result[i]=-1;
+        }
+        int count=0;
+        int profit=0;
+        for(int i=0;i<arr.length;i++){
+                int j=arr[i].deadline;
+                while(j>0){
+                    if(result[j]==-1){
+                        result[j]=arr[i].id;
+                        count++;
+                        profit+=arr[i].profit;
+                        break;
+                    }
+                    j--;
+                }
+            }
+        int ans[]=new int[2];
+        ans[0]=count;
+        ans[1]=profit;
+        return ans;
+    }
+}
