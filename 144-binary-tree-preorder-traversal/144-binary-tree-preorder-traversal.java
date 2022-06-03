5483a84eb80d019d74561a702c20a9eb33f8184e
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-06-03 19:33:17 +0530

    Time: 1 ms (44.01%), Space: 42.3 MB (44.02%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -15,18 +15,21 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+        //first print and go to left till you get the null with pushing on stack
+        //as soon as got a null pop from stack and pointer point to the right
+        //do this same as till in inorder i.e., **!stack.isEmpty()||curr!=null**
         List<Integer>list=new ArrayList<>();
         Stack<TreeNode>stack=new Stack<>();
         TreeNode curr = root;
         while(!stack.isEmpty()||curr!=null){
-            while(curr!=null){
+            if(curr!=null){
                 list.add(curr.val);
                 stack.push(curr);
                 curr=curr.left;
-            }
+            }else{
                 TreeNode node = stack.pop();
                 curr=node.right;
-            
+            }
         }
         return list;
     }
