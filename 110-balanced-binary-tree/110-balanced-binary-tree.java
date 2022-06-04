1acd78131155d3eb397b81d3ec44d0b1b153568e
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-06-04 10:32:52 +0530

    Time: 1 ms (96.70%), Space: 43.6 MB (69.78%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -15,15 +15,27 @@
  */
 class Solution {
     
+    
+//if we separately call the recursion for left & right after checking for root only it will be O(n*h) ~ O(n*n) for skew tree.
+        
+   //OPTIMIZATION O(n*n)~O(n):-
+   //In the height function itself check the condition if leftheight-rightheight is greater than 1 then return -1
+   //and while calling left and right recursively check if either of the result is -1 then just return -1
+   //In our final function check if height==-1 return false else return true
+    
+    
     public int height(TreeNode root){
         if(root==null){
             return 0;
         }
+        
+        // if either of the result is -1 return -1;
         int leftHeight = height(root.left);
         if(leftHeight==-1)return -1;
         int rightHeight = height(root.right);
         if(rightHeight==-1)return -1;
         
+        //if difference of height is greater than 1 return -1 instead of height;
         if(Math.abs(leftHeight-rightHeight)>1){
             return -1;
         }
@@ -36,6 +48,5 @@ class Solution {
             return false;
         }
         return true;
-        
     }
 }
