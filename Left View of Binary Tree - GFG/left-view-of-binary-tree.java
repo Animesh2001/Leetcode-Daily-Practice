f4fdd9906d85f1e6c0728920711da5bcc1d2072c
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-06-05 06:41:41 +0530

    Added solution - LeetHub

---
+++ Left View of Binary Tree - GFG/left-view-of-binary-tree.java
@@ -0,0 +1,153 @@
+// { Driver Code Starts
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Tree g = new Tree();
+    			ArrayList<Integer> result = g.leftView(root);
+    			for(int value : result){
+    			    System.out.print(value + " ");
+    			}
+    			System.out.println();
+                t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+class Tree
+{
+    class Pair{
+        Node node;
+        int i;
+        
+        public Pair(Node node,int i){
+            this.node=node;
+            this.i=i;
+        }
+    }
+    //Function to return list containing elements of left view of binary tree.
+    ArrayList<Integer> leftView(Node root)
+    {
+      // Your code here
+      ArrayList<Integer>list=new ArrayList<>();
+      Queue<Pair>queue=new LinkedList<>();
+      Map<Integer,Integer>map=new TreeMap<>();
+      if(root==null)return list;
+      queue.add(new Pair(root,0));
+      while(!queue.isEmpty()){
+          Pair p = queue.poll();
+          if(!map.containsKey(p.i))map.put(p.i,p.node.data);
+          if(p.node.left!=null)queue.add(new Pair(p.node.left,p.i+1));
+          if(p.node.right!=null)queue.add(new Pair(p.node.right,p.i+1));
+      }
+      for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+          list.add(entry.getValue());
+      }
+      return list;
+    }
+}
