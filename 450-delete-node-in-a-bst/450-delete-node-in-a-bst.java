d06ebf5793d1a21184e1bf354528d5fbe9d316ef
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-06-12 14:56:41 +0530

    Time: 0 ms (100.00%), Space: 49 MB (68.77%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -15,73 +15,35 @@
  */
 class Solution {
     
-    public TreeNode searchforparent(TreeNode root,int key){
-        if(root==null){
-            return null;
-        }
-        
-        if(root.left!=null&&root.left.val==key){
-            return root;
-        }
-        
-        if(root.right!=null&&root.right.val==key){
-            return root;
-        }
-        
-        TreeNode parent = searchforparent(root.left,key);
-        return parent==null?searchforparent(root.right,key):parent;
-        
-    }
-    
-//     public TreeNode insert(TreeNode root,int key){
-        
-//     }
+    public int getMax(TreeNode root){
+       if(root.right!=null){
+          return getMax(root.right);
+       }else{
+           return root.val;
+       }
+    } 
     
     public TreeNode deleteNode(TreeNode root, int key) {
-         if(root==null)return null;
+        if(root==null)return null;
         
-        if(root.val==key){
-             if(root.left!=null){
-                    TreeNode temp=root.left;
-                    while(temp.right!=null){
-                        temp=temp.right;
-                    }
-                    temp.right=root.right;
-                    root=root.left;
-                }
-                else{
-                    root=root.right;
-                }
+        if(root.val>key){
+            root.left=deleteNode(root.left,key);
+        }else if(root.val<key){
+            root.right=deleteNode(root.right,key);
         }
-        
-        
-        TreeNode parent = searchforparent(root,key);
-        if(parent!=null){
-            if(parent.left!=null&&parent.left.val==key){
-                if(parent.left.left!=null){
-                    TreeNode temp=parent.left.left;
-                    while(temp.right!=null){
-                        temp=temp.right;
-                    }
-                    temp.right=parent.left.right;
-                    parent.left=parent.left.left;
-                }
-                else{
-                    parent.left=parent.left.right;
-                }
-            }
-            else if(parent.right.val==key){
-                 if(parent.right.left!=null){
-                    TreeNode temp=parent.right.left;
-                    while(temp.right!=null){
-                        temp=temp.right;
-                    }
-                    temp.right=parent.right.right;
-                    parent.right=parent.right.left;
-                }
-                else{
-                    parent.right=parent.right.right;
-                }
+        else{
+            //work
+            if(root.left!=null&&root.right!=null){
+               int lmax = getMax(root.left);
+               root.val=lmax;
+               root.left=deleteNode(root.left,lmax);
+               return root;
+            }else if(root.left!=null){
+                return root.left;
+            }else if(root.right!=null){
+                return root.right;
+            }else{
+                return null;
             }
         }
         return root;
