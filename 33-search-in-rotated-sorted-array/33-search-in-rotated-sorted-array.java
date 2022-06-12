bcc1e1f188fd786879d51b4445e74628bf1b7cfd
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-06-12 21:05:41 +0530

    Time: 1 ms (62.74%), Space: 42.5 MB (53.91%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,7 +1,8 @@
 class Solution {
     public int search(int[] nums, int target) {
         if(nums.length==1){
-            return target==nums[0]?0:-1;
+            if(target==nums[0])return 0;
+            else return -1;
         }
         int ans = findmin(nums);
         int ans1 = search(nums,target,0,ans-1);
@@ -9,32 +10,25 @@ class Solution {
         return ans1==-1?ans2:ans1;
     }
     
-     public int findmin(int nums[]){
+    public int findmin(int nums[]){
         int start = 0;
         int end = nums.length-1;
-       
-       if(nums.length==1){
-           return nums[0];
-       }
-       
-       int ans=-1;
-       int min=Integer.MAX_VALUE;
         
         while(start<=end){
             int mid = start+(end-start)/2;
-            if(min>nums[mid]){
-                ans=mid;
-                min=nums[mid];
+            int next = (mid+1)%nums.length;
+            int prev = (mid+nums.length-1)%nums.length;
+            if(nums[mid]<nums[next]&&nums[mid]<nums[prev]){
+                return mid;
             }
-            
-            if(nums[mid]>nums[end]){
-                start=mid+1;
-            }
-            else{
+            else if(nums[mid]<nums[end]){
                 end=mid-1;
             }
+            else{
+                start=mid+1;
+            }
         }
-        return ans;
+        return -1;
     }
     
     public int search(int nums[], int target, int start, int end){
