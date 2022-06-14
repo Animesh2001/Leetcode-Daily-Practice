9d14f3695eb2bb79727e98f163beee4296c33e98
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-06-14 18:00:27 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (98.29%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -15,22 +15,29 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        //Make a stack and traverse to the left till you get a null
-        //as soon as you get the null pop from stack and print it with moving pointer to right of pop element
-        //repeat this steps till your **!stack.isEmpty()||pointer!=null**
-         List<Integer>list=new ArrayList<>();
-         TreeNode curr = root;
-         Stack<TreeNode>stack=new Stack<>();
-        while(curr!=null||!stack.isEmpty()){
-            if(curr!=null){
-                stack.push(curr);
-                curr=curr.left;
-            }else{
-                TreeNode node = stack.pop();
-                list.add(node.val);
-                curr=node.right;
+        List<Integer>inorder=new ArrayList<>();
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left==null){
+                inorder.add(curr.val);
+                curr=curr.right;
+            }
+            else{
+                TreeNode temp=curr.left;
+                while(temp.right!=null&&temp.right!=curr)
+                {
+                    temp=temp.right;
+                }
+                if(temp.right==null){
+                    temp.right=curr;
+                    curr=curr.left;
+                }else{
+                    temp.right=null;
+                    inorder.add(curr.val);
+                    curr=curr.right;
+                }
             }
         }
-        return list;
+        return inorder;
     }
 }
