13c3efb156b8f54db7e240eb61ec4d95c93cbc33
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-06-17 18:23:55 +0530

    Added solution - LeetHub

--- Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.java
+++ Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.java
@@ -83,28 +83,24 @@ class Sol
    public static Node findIntersection(Node head1, Node head2)
     {
         // code here.
-        Node temp = null;
-        Node head = null;
-        Node temp1 = head1, temp2 = head2;
-        
+        Node temp1 = head1;
+        Node temp2 = head2;
+        Node newhead=null,tail=null;
         while(temp1!=null&&temp2!=null){
-            if(temp1.data==temp2.data){
-                Node newNode = new Node(temp1.data);
-                if(temp==null){
-                    temp=newNode;
-                    head=newNode;
+            if(temp1.data<temp2.data)temp1=temp1.next;
+            else if(temp1.data>temp2.data)temp2=temp2.next;
+            else{
+                if(newhead==null){
+                    newhead=new Node(temp1.data);
+                    tail=newhead;
                 }else{
-                    temp.next=newNode;
-                    temp=temp.next;
+                    tail.next=new Node(temp1.data);
+                    tail=tail.next;
                 }
                 temp1=temp1.next;
-            }
-            else if(temp1.data<temp2.data){
-                temp1=temp1.next;
-            }else{
                 temp2=temp2.next;
             }
         }
-        return head;
+        return newhead;
     }
 }
