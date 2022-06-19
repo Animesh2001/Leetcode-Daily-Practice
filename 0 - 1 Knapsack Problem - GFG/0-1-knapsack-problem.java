307c1430ae7301ca1531f4d1b06c1fa5c91c5652
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-06-19 11:30:41 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -48,18 +48,23 @@ class gfg
 class Solution 
 { 
     static int knapSack(int w, int wt[], int val[], int n,int[][]t){ 
-        if(n==0||w==0){
-             return 0;
-         }
-         
-         if(t[n][w]!=-1)return t[n][w];
-         
-         if(wt[n-1]<=w){
-           return t[n][w]= Math.max(val[n-1]+knapSack(w-wt[n-1],wt,val,n-1,t),knapSack(w,wt,val,n-1,t));
-         }
-         else{
-             return t[n][w]= knapSack(w,wt,val,n-1,t);
-         }
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<w+1;j++){
+                if(i==0||j==0)t[i][j]=0;
+            }
+        }
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<w+1;j++){
+                if(wt[i-1]<=j){
+                    t[i][j]=Math.max(val[i-1]+t[i-1][j-wt[i-1]],t[i-1][j]);
+                }else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][w];
+        
     }
     
     //Function to return max value that can be put in knapsack of capacity W.
@@ -68,11 +73,7 @@ class Solution
          // your code here
          
          int t[][]=new int[n+1][w+1];
-         for(int i=0;i<n+1;i++){
-             for(int j=0;j<w+1;j++){
-                 t[i][j]=-1;
-             }
-         }
+        
          return knapSack(w,wt,val,n,t);
          
     } 
