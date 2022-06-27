9e3d46e7db367b2509c382bed3462e98e860f7e5
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-06-27 12:55:09 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (89.36%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,5 +1,6 @@
 class Solution {
-    
+   
+    //same code of strivers's lec5
     public int f(int nums[]){
         int prev=nums[0];
         int prev2=0;
@@ -16,15 +17,20 @@ class Solution {
         
     }
     
+    
     public int rob(int[] nums) {
-        
         int temp1[] = new int[nums.length-1];
         int temp2[] = new int[nums.length-1];
+        
+        //ALERT= CHECK THIS STEP IF ITS OF SIZE 1
         if(nums.length==1)return nums[0];
+        
+        
         for(int i=0;i<nums.length;i++){
             if(i!=0)temp1[i-1]=nums[i];
             if(i!=nums.length-1)temp2[i]=nums[i];
         }
+        //here only first and last element should not be adjacent
         return Math.max(f(temp1),f(temp2));
         
     }
