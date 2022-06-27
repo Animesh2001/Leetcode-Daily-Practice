e06399a9b3127f033ea250312f4aacc24db6773d
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-06-27 12:53:49 +0530

    Time: 1 ms (35.49%), Space: 41.6 MB (39.99%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    
+    public int f(int nums[]){
+        int prev=nums[0];
+        int prev2=0;
+        
+        for(int i=1;i<nums.length;i++){
+            int take = nums[i];
+            if(i>1)take+=prev2;
+            int nontake = 0+prev;
+            int curri=Math.max(take,nontake);
+            prev2=prev;
+            prev=curri;
+        }
+        return prev;
+        
+    }
+    
+    public int rob(int[] nums) {
+        
+        int temp1[] = new int[nums.length-1];
+        int temp2[] = new int[nums.length-1];
+        if(nums.length==1)return nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(i!=0)temp1[i-1]=nums[i];
+            if(i!=nums.length-1)temp2[i]=nums[i];
+        }
+        return Math.max(f(temp1),f(temp2));
+        
+    }
+}
