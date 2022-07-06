3e216b6d85135f39b59111be14301477e9ae065e
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-07-06 17:04:01 +0530

    Time: 69 ms (19.00%), Space: 48.2 MB (62.53%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -29,11 +29,44 @@ class Solution {
         int m = s.length();
         int n= p.length();
         
-        int dp[][]=new int[n+1][m+1];
-        for(int rows[]:dp){
-            Arrays.fill(rows,-1);
+        //memoization
+        boolean dp[][]=new boolean[n+1][m+1];
+//         for(int rows[]:dp){
+//             Arrays.fill(rows,-1);
+//         }
+        
+//         return f(p,s,n,m,dp)==1;
+        
+        //tabulation 
+        dp[0][0]=true;
+        for(int i=1;i<n+1;i++){
+          boolean flag=true;
+            for(int k=1;k<=i;k++){
+                if(p.charAt(k-1)!='*'){
+                   flag = false;
+                }
+            }
+            dp[i][0]=flag;
+        }
+        for(int j=1;j<m+1;j++){
+            dp[0][j]=false;
         }
         
-        return f(p,s,n,m,dp)==1;
-    }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+          if(p.charAt(i-1)==s.charAt(j-1)||p.charAt(i-1)=='?'){
+           dp[i][j]= dp[i-1][j-1];
+        }
+        //ALERT : You need to consider not taking '*' and then taking it.
+        //Watch Strivers video once for this step.
+          else if(p.charAt(i-1)=='*'){
+              dp[i][j]= dp[i-1][j]||dp[i][j-1];
+        }
+          else dp[i][j]=false;
+            
+          }
+        }
+        return dp[n][m];
+        
+}
 }
