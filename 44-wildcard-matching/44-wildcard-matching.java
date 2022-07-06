dc315e0c4e9f65c7a377a666d4b4e52219fa5095
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-07-06 16:35:37 +0530

    Time: 53 ms (33.40%), Space: 54.5 MB (14.17%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -0,0 +1,42 @@
+class Solution {
+    
+    public int f(String s, String p, int i,int j,int dp[][]){
+        if(i==0&&j==0)return 1;
+        if(i==0&&j>0)return 0;
+        if(i>0&&j==0){
+            for(int k=0;k<i;k++){
+                if(s.charAt(k)!='*')return dp[i][j]=0;
+            }
+            return 1;
+        }
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s.charAt(i-1)==p.charAt(j-1)||s.charAt(i-1)=='?'){
+            if(f(s,p,i-1,j-1,dp)==1){
+               return dp[i][j]=1;
+            }else{
+               return dp[i][j]=0;
+            }
+        }
+        //ALERT : You need to consider not taking '*' and then taking it.
+        if(s.charAt(i-1)=='*'){
+            if(f(s,p,i-1,j,dp)==1||f(s,p,i,j-1,dp)==1){
+              return  dp[i][j]=1;
+            }
+            return dp[i][j]=0;
+        }
+        return 0;
+    }
+    
+    public boolean isMatch(String s, String p) {
+        
+        int n = s.length();
+        int m = p.length();
+        
+        int dp[][]=new int[m+1][n+1];
+        for(int rows[]:dp){
+            Arrays.fill(rows,-1);
+        }
+        
+        return f(p,s,m,n,dp)==1;
+    }
+}
