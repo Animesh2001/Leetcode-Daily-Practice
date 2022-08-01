e32c6f092dc452a26a3d43e0d510dcf4289ef780
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-02 00:13:25 +0530

    Time: 1 ms (55.39%), Space: 41.7 MB (10.24%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,17 +1,33 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0||j==0){
-                    dp[i][j]=1;
-                }
-                else {
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                }
-            }
+    
+    public int uniquePaths(int m,int n, int i, int j ,int matrix[][]){
+        //base case
+        if(i==m-1&&j==n-1){
+            return matrix[i][j]= 1;    
         }
-        return dp[m-1][n-1];
+        if(matrix[i][j]!=-1)return matrix[i][j];
+        //recursive calls
+        int right =0,bottom=0;
         
+        if(j+1<n){
+           right = uniquePaths(m,n,i,j+1,matrix);
+        }
+        
+        if(i+1<m){
+          bottom = uniquePaths(m,n,i+1,j,matrix);
+        }
+        
+        return matrix[i][j]=right+bottom;
+        
+    }
+    
+    public int uniquePaths(int m, int n) {
+        
+        int matrix[][]=new int[m+1][n+1];
+        
+        for(int rows[]:matrix){
+            Arrays.fill(rows,-1);
+        }
+       return  uniquePaths(m,n,0,0,matrix);
     }
 }
