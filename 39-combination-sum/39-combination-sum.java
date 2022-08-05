16284a51836fc50611f75dbcd6abd6ff8f7e390a
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-05 12:52:47 +0530

    Time: 3 ms (95.48%), Space: 45.1 MB (69.91%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,36 +1,28 @@
 class Solution {
-    public void combination(int i,int s,int target,int arr[],List<List<Integer>>ans,List<Integer>list,int n){
-        if(i>=n||s>=target){
-            if(s==target){
-                List<Integer>newList=new ArrayList<>();
-                for(int e:list){
-                    newList.add(e);
-                }
-                ans.add(newList);
+    public void combination(int i,int target,int []candidates,List<List<Integer>>ans,List<Integer>list){
+        int n=candidates.length;
+        if(i>=n){
+            if(target==0){
+                ans.add(new ArrayList<>(list));
             }
             return;
         }
-        // list=new ArrayList<Integer>();
-        if(s+arr[i]<=target){
-            list.add(arr[i]);
-            s+=arr[i];
-            combination(i,s,target,arr,ans,list,n);
-            list.remove(Integer.valueOf(arr[i]));
-            s-=arr[i];
+        
+        if(candidates[i]<=target){
+            list.add(candidates[i]);
+            combination(i,target-candidates[i],candidates,ans,list);
+            list.remove(list.size()-1);
         }
         
-        combination(i+1,s,target,arr,ans,list,n);
-       
-         
-        
+        combination(i+1,target,candidates,ans,list);
         
     }
     
-    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>>ans=new ArrayList<>();
-        List<Integer>list=new ArrayList<>();
-        combination(0,0,target,candidates,ans,list,candidates.length);
+        
+        combination(0,target,candidates,ans,new ArrayList<Integer>());
         return ans;
+        
     }
 }
