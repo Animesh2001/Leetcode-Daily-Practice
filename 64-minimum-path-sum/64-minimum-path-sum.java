2cf4b3eb3bb4540419b8e23413335e1006bea02a
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-11 19:16:33 +0530

    Time: 2 ms (90.49%), Space: 45.7 MB (65.73%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -8,15 +8,9 @@ class Solution {
         
         
       
-        // int x = grid[n][m]+;
-        // int y = grid[n][m]+;
-        return dp[n][m]=grid[n][m]+Math.min(minSumPath(n-1,m,grid,dp),minSumPath(n,m-1,grid,dp));
-        
-        // int ans = Math.min(x,y) + grid[n][m];
-        // if(ans==Integer.MAX_VALUE) return grid[n][m];
-        // dp[n][m] = ans;
-        // return ans;
-        // return dp[n][m]= Math.min(up,left);
+        int up = minSumPath(n-1,m,grid,dp);
+        int left = minSumPath(n,m-1,grid,dp);
+        return dp[n][m]=grid[n][m]+Math.min(up,left);
     }
     
     public int minPathSum(int[][] grid) {
@@ -29,29 +23,4 @@ class Solution {
         return minSumPath(n-1,m-1,grid,dp);
     }
 }
-// class Solution 
-// {
-//     static int[][] memo;
-//     public int minPathSum(int[][] grid) 
-//     {
-//         int m = grid.length-1;
-//         int n = grid[0].length-1;
-        
-//         memo = new int[m+1][n+1];
-        
-//         return find(grid, m, n, memo);
-//     }
-//     public int find(int grid[][], int m, int n, int[][] memo)
-//     {
-//         if(m==0 && n==0)
-//             return grid[0][0];
-        
-//         else if(m<0 || n<0)
-//             return Integer.MAX_VALUE;
-        
-//         else if(memo[m][n]!=0)
-//             return memo[m][n];
-//         else
-//             return memo[m][n] =  grid[m][n] + Math.min(find(grid, m-1, n, memo),find(grid, m, n-1, memo)); 
-// }
-//     }
+ 
