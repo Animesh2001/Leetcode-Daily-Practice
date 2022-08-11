afe461971986bad0a576096e2534ffe88b20b860
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-11 16:36:18 +0530

    Time: 1 ms (55.29%), Space: 40.1 MB (85.00%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -27,9 +27,9 @@ class Solution {
                 if(i==0&&j==0)dp[i][j]=1;
                 else{
                 int up=0,left=0;
-                if(i>0)
+                if(i-1>=0)
                  up = dp[i-1][j];
-                if(j>0)
+                if(j-1>=0)
                  left = dp[i][j-1];
                 dp[i][j]=up+left;
             }
