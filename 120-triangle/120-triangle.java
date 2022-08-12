74b298321de1b4b097314954861a47b9a9621da2
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-12 11:09:33 +0530

    Time: 2 ms (94.74%), Space: 44.7 MB (16.54%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -17,20 +17,20 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int dp[][]=new int[triangle.size()][triangle.get(triangle.size()-1).size()];
-        for(int rows[]:dp){
-            Arrays.fill(rows,-1);
-        }
-        return minimumTotal(triangle,0,0,dp);
-        
-        // int row=triangle.size(),col=triangle.get(triangle.size()-1).size();
-        // for(int i=row-1;i>=0;i--){
-        //     for(int j=0;j<=i;j++){
-        //         if(i==row-1){dp[i][j]=triangle.get(i).get(j);}
-        //         else{
-        //             dp[i][j]=triangle.get(i).get(j)+Math.min(dp[i+1][j],dp[i+1][j+1]);
-        //         }
-        //     }
+        // for(int rows[]:dp){
+        //     Arrays.fill(rows,-1);
         // }
-        // return dp[0][0];
+        // return minimumTotal(triangle,0,0,dp);
+        
+        int row=triangle.size(),col=triangle.get(triangle.size()-1).size();
+        for(int i=row-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                if(i==row-1){dp[i][j]=triangle.get(i).get(j);}
+                else{
+                    dp[i][j]=triangle.get(i).get(j)+Math.min(dp[i+1][j],dp[i+1][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
