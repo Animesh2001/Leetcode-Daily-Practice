297b7d929d427b4d3d9f28322657bb648d928420
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-12 20:47:42 +0530

    Time: 40 ms (76.22%), Space: 72.3 MB (12.86%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,45 +1,30 @@
 class Solution {
     
-      public int subset(int n,int []arr,int sum){
-        int t[][]=new int[n+1][sum+1];
-        
-        //initialize the matrix
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-                if(i==0){
-                    t[i][j]=0;
-                }
-                if(j==0){
-                    t[i][j]=1;
-                }
+    public int findSubset(int n,int []nums,int target,int[][]dp){
+       if(n==0){
+           if(target==0)return 1;
+           else return 0;
+       }
+        if(target==0)return 1;
+        if(dp[n][target]!=-1)return dp[n][target];
+        if(nums[n]<=target){
+            if(findSubset(n-1,nums,target-nums[n],dp)!=0||findSubset(n-1,nums,target,dp)!=0){
+                return dp[n][target]=1;
             }
         }
-        
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<sum+1;j++){
-                if(arr[i-1]<=j){
-                   if(t[i-1][j-arr[i-1]]!=0||t[i-1][j]!=0){
-                       t[i][j]=1;
-                   }else{
-                       t[i][j]=0;
-                   }
-                }
-                else{
-                    t[i][j]=t[i-1][j];
-                }
-            }
-        }
-        return t[n][sum];
-        
+        else if(findSubset(n-1,nums,target,dp)!=0)return dp[n][target]=1;
+        return dp[n][target]=0;
     }
+    
     public boolean canPartition(int[] nums) {
-         int sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
+        int sum=0;
+        for(int i:nums){
+            sum+=i;
         }
-        
         if(sum%2==0){
-            return subset(nums.length,nums,sum/2)==1?true:false;
+            int dp[][]=new int[nums.length][sum/2+1];
+            for(int rows[]:dp)Arrays.fill(rows,-1);
+            return findSubset(nums.length-1,nums,sum/2,dp)==1;
         }
         return false;
     }
