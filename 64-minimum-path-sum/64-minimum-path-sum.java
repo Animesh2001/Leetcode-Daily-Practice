eb5291078969c3d8c55c0831d427f4abe76b0097
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-12 10:54:10 +0530

    Time: 5 ms (21.24%), Space: 46 MB (42.27%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -13,19 +13,22 @@ class Solution {
         int row=grid.length-1;
         int col=grid[0].length-1;
         int dp[][]=new int[row+1][col+1];
-        for(int rows[]:dp){
-            Arrays.fill(rows,-1);
-        }
-        return minPathSum(0,0,row,col,grid,dp);
-        
-        // for(int i=row;i>=0;i--){
-        //     for(int j=col;j>=0;j--){
-        //         if(i==row&&j==col)dp[i][j]=grid[i][j];
-        //         else{
-        //             dp[i][j]= grid[i][j]+Math.min(dp[i+1][j],dp[i][j+1]);
-        //         }
-        //     }
+        // for(int rows[]:dp){
+        //     Arrays.fill(rows,-1);
         // }
-        // return dp[0][0];
+        // return minPathSum(0,0,row,col,grid,dp);
+        
+        for(int i=row;i>=0;i--){
+            for(int j=col;j>=0;j--){
+                if(i==row&&j==col)dp[i][j]=grid[i][j];
+                else{
+                    int down = Integer.MAX_VALUE,right=Integer.MAX_VALUE;
+                    if(i<row)down=dp[i+1][j];
+                    if(j<col)right=dp[i][j+1];
+                    dp[i][j]=grid[i][j]+Math.min(down,right);
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
