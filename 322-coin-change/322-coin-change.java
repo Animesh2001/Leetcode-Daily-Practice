8742f5dc13afa8a91d5ed882ddc116f7b673fbad
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-13 20:08:48 +0530

    Time: 30 ms (53.43%), Space: 42 MB (93.04%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,25 @@
+class Solution {
+        public static int minimum(int ind,int arr[],int target,int dp[][]){
+//         if(target==0)return 0;
+        if(ind==0){
+            if(target%arr[0]==0)return target/arr[0];
+            else return Integer.MAX_VALUE-1;
+        }
+        if(dp[ind][target]!=-1)return dp[ind][target];
+        int pick=Integer.MAX_VALUE-1;
+        if(arr[ind]<=target){
+            pick=1+minimum(ind,arr,target-arr[ind],dp);
+        }
+        int notpick = minimum(ind-1,arr,target,dp);
+        return dp[ind][target]= Math.min(pick,notpick);
+    }
+    
+    public int coinChange(int[] num, int x) {
+         int dp[][]=new int[num.length][x+1];
+        for(int rows[]:dp)Arrays.fill(rows,-1);
+        
+        int ans = minimum(num.length-1,num,x,dp);
+        if(ans>x)return -1;
+        return ans;
+    }
+}
