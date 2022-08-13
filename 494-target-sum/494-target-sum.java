46451df08daa7c402544f3a7c099612baccaef5c
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-13 22:29:14 +0530

    Time: 9 ms (80.65%), Space: 44 MB (39.63%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,18 +1,43 @@
 class Solution {
-    
-    public int findTargetSumWays(int[] A, int target,int idx,int sum){
-        if(idx==A.length){
-            if(target==sum){
-             return 1;       
+    public int count(int arr[],int target){
+        int dp[][]=new int[arr.length][target+1];
+        
+        for(int i=0;i<=target;i++){
+            if(arr[0]==0&&i==0){
+                dp[0][i]=2;
             }
+            else if(i==0||arr[0]==i){
+                dp[0][i]=1;
+            }
+            else{
+                dp[0][i]=0;
+            }
+        }
+        
+        for(int i=1;i<arr.length;i++){
+            for(int j=0;j<=target;j++){
+                int pick=0;
+                if(arr[i]<=j){
+                    pick=dp[i-1][j-arr[i]];
+                }
+                int notpick=dp[i-1][j];
+                dp[i][j]=pick+notpick;
+            }
+        }
+        return dp[arr.length-1][target];
+        
+    }
+    
+    
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum=0;
+        for(int i:nums){
+            sum+=i;
+        }
+        if(sum-target<0||(sum-target)%2!=0){
             return 0;
         }
-        // int a1=0,a2=0;
-       int  a1=findTargetSumWays(A,target,idx+1,sum+A[idx]);
-        int a2=findTargetSumWays(A,target,idx+1,sum-A[idx]);
-        return a1+a2;
-    } 
-    public int findTargetSumWays(int[] nums, int target) {
-       return findTargetSumWays(nums,target,0,0);
+        int x = (sum-target)/2;
+        return count(nums,x);
     }
 }
