d30c48643c8a2812f4eee02970f444f5b9b0b94f
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-15 11:19:01 +0530

    Time: 23 ms (72.46%), Space: 50.3 MB (37.38%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,43 +1,20 @@
 class Solution {
     
-    public int f(String s,String t,int i,int j,int [][]dp){
-        if(j==0)return 1;
-        if(i==0)return 0;
+    public int count(int i,int j,String str1, String str2,int dp[][]){
+        if(i==0&&j>0)return 0;
+        if(i>=0&&j==0)return 1;
         if(dp[i][j]!=-1)return dp[i][j];
-        if(s.charAt(i-1)==t.charAt(j-1)){
-            return dp[i][j]= f(s,t,i-1,j-1,dp)+f(s,t,i-1,j,dp);
-        }else{
-            return dp[i][j]= f(s,t,i-1,j,dp);
+        if(str2.charAt(j-1)==str1.charAt(i-1)){
+            return dp[i][j]=count(i-1,j-1,str1,str2,dp)+count(i-1,j,str1,str2,dp);
         }
+        return dp[i][j]=count(i-1,j,str1,str2,dp);
     }
     
     public int numDistinct(String s, String t) {
-        int dp[][]=new int[s.length()+1][t.length()+1];
-        // for(int rows[]:dp){
-        //     Arrays.fill(rows,-1);
-        // }
-        // return f(s,t,s.length(),t.length(),dp);
-        
-        for(int i=0;i<=s.length();i++){
-            dp[i][0]=1;
-        }
-        
-        for(int j=1;j<=t.length();j++){
-            dp[0][j]=0;
-        }
-        
-        for(int i=1;i<s.length()+1;i++){
-            for(int j=1;j<t.length()+1;j++){
-                if(s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                }else{
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        
-        return dp[s.length()][t.length()];
-        
-        
+        int n = s.length();
+        int m = t.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int rows[]:dp)Arrays.fill(rows,-1);
+        return count(n,m,s,t,dp);
     }
 }
