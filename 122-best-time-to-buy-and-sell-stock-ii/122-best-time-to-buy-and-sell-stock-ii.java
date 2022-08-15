dd30382fe6c6cf664495d52389db97cc06327d95
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-15 18:21:41 +0530

    Time: 6 ms (7.95%), Space: 48.9 MB (5.07%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,38 +1,28 @@
 class Solution {
     
-    public static int f(int ind,int buy,int n,int []arr,int dp[][]){
-        if(ind==n)return 0;
-        if(dp[ind][buy]!=-1)return dp[ind][buy];
-        int profit = 0;
+    public int profit(int ind,int []prices,int buy,int dp[][]){
+        //base case
+        if(ind==prices.length)return 0;
         
-        //you can buy
+        int profit=0;
+        if(dp[ind][buy]!=-1)return dp[ind][buy];
+        
+        //recursive calls & small calculations
         if(buy==1){
-            profit = Math.max(-arr[ind]+f(ind+1,0,n,arr,dp),0+f(ind+1,1,n,arr,dp));
-        }else{
-            profit = Math.max(arr[ind]+f(ind+1,1,n,arr,dp),0+f(ind+1,0,n,arr,dp));
+            profit=Math.max(-prices[ind]+profit(ind+1,prices,0,dp),0+profit(ind+1,prices,1,dp));
         }
-        return dp[ind][buy]=profit;
+        else{
+            profit=Math.max(prices[ind]+profit(ind+1,prices,1,dp),0+profit(ind+1,prices,0,dp));
+        }
+        
+        //return the maxprofit
+        return dp[ind][buy]= profit;
     }
     
-    public int maxProfit(int[] arr) {
-        int n = arr.length;
-        int dp[][]=new int[n+1][2];
-     
-        dp[n][0]=0;
-        dp[n][1]=0;
-        
-         for(int i=n-1;i>=0;i--){
-             for(int j=0;j<2;j++){
-                 if(j==1){
-                     dp[i][j]=Math.max(-arr[i]+dp[i+1][0],dp[i+1][1]);
-                 }else{
-                     dp[i][j]=Math.max(arr[i]+dp[i+1][1],dp[i+1][0]);
-                 }
-             }
-         }
-        
-        return dp[0][1];
-        
-        
+    
+    public int maxProfit(int[] prices) {
+        int dp[][]=new int[prices.length][2];
+        for(int rows[]:dp)Arrays.fill(rows,-1);
+        return profit(0,prices,1,dp);
     }
 }
