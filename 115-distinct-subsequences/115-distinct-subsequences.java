306740b6f520fa1ee9c526032b33c7a8a04e2f11
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-15 11:24:28 +0530

    Time: 29 ms (53.49%), Space: 50.3 MB (46.19%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -14,7 +14,22 @@ class Solution {
         int n = s.length();
         int m = t.length();
         int dp[][]=new int[n+1][m+1];
-        for(int rows[]:dp)Arrays.fill(rows,-1);
-        return count(n,m,s,t,dp);
+        // for(int rows[]:dp)Arrays.fill(rows,-1);
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int j=1;j<=m;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(t.charAt(j-1)==s.charAt(i-1)){
+                     dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n][m];
+        // return count(n,m,s,t,dp);
     }
 }
