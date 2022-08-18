3cceb25baf1547424fd4d737bf0e5c9a71bb34b0
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-18 16:47:01 +0530

    Time: 51 ms (27.95%), Space: 54.2 MB (82.72%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -1,34 +1,19 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int arr[]=new int[2*nums.length];
-        int i=0;
-        for(;i<nums.length;i++){
-            arr[i]=nums[i];
-        }
-        int k=0;
-        for(;i<arr.length;i++){
-            arr[i]=nums[k++];
-        }
-        
-        int ans[]=new int[2*nums.length];
         Stack<Integer>stack=new Stack<>();
-        for(int j=arr.length-1;j>=0;j--){
-            while(stack.size()>0&&stack.peek()<=arr[j]){
+        int ans[]=new int[nums.length];
+        int n=nums.length;
+        for(int i=2*nums.length-1;i>=0;i--){
+            while(stack.size()>0&&stack.peek()<=nums[i%n]){
                 stack.pop();
             }
             if(stack.size()==0){
-                ans[j]=-1;
+                ans[i%n]=-1;
+            }else{
+                ans[i%n]=stack.peek();
             }
-            else{
-                ans[j]=stack.peek();
-            }
-            stack.push(arr[j]);
+            stack.push(nums[i%n]);
         }
-        
-        for(int j=0;j<nums.length;j++){
-            nums[j]=ans[j];
-        }
-        return nums;
-        
+        return ans;
     }
 }
