565689cad3ff6b90203c45ea90f2a9fecfa039f1
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-18 16:48:15 +0530

    Time: 55 ms (19.15%), Space: 54.5 MB (73.06%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -7,10 +7,12 @@ class Solution {
             while(stack.size()>0&&stack.peek()<=nums[i%n]){
                 stack.pop();
             }
+            if(i<n){
             if(stack.size()==0){
-                ans[i%n]=-1;
+                ans[i]=-1;
             }else{
-                ans[i%n]=stack.peek();
+                ans[i]=stack.peek();
+            }
             }
             stack.push(nums[i%n]);
         }
