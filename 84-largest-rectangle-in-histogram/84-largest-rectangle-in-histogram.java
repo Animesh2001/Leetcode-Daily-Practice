2b112b29c0b8f7686df341a303406a80c49de6c1
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-19 13:31:10 +0530

    Time: 272 ms (33.48%), Space: 57.5 MB (92.96%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,61 +1,48 @@
 class Solution {
     
-    public static class Pair{
-        int num;
-        int index;
-        public Pair(int num,int index){
-            this.num=num;
-            this.index=index;
+    public int[] getnsl(int heights[]){
+        Stack<Integer>stack=new Stack<>();
+        int nsl[]=new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            while(stack.size()>0&&heights[stack.peek()]>=heights[i]){
+                stack.pop();
+            }
+            if(stack.size()>0){
+                nsl[i]=stack.peek();
+            }else{
+                nsl[i]=-1;
+            }
+            stack.push(i);
         }
+        return nsl;
     }
     
-    //Function to find largest rectangular area possible in a given histogram.
-      public int largestRectangleArea(int[] hist) {
-    {
-        // your code here
-        Stack<Pair>stack=new Stack<>();
-        ArrayList<Integer>nsr=new ArrayList<>();
-        ArrayList<Integer>nsl=new ArrayList<>();
-        
-        for(int i=hist.length-1;i>=0;i--){
-            if(stack.isEmpty())nsr.add(hist.length);
-            else if(stack.peek().num<hist[i])nsr.add(i+1);
-            else if(stack.peek().num>=hist[i]){
-                while(!stack.isEmpty()&&stack.peek().num>=hist[i]){
-                    stack.pop();
-                }
-                if(stack.isEmpty())nsr.add(hist.length);
-                else nsr.add(stack.peek().index);
+    
+    public int[] getnsr(int[] heights){
+        Stack<Integer>stack=new Stack<>();
+        int nsr[]=new int[heights.length];
+        for(int i=heights.length-1;i>=0;i--){
+            while(stack.size()>0&&heights[stack.peek()]>=heights[i]){
+                stack.pop();
             }
-            stack.push(new Pair(hist[i],i));
-        }
-        Collections.reverse(nsr);
-        
-        stack=new Stack<>();
-        for(int i=0;i<hist.length;i++){
-            if(stack.isEmpty())nsl.add(-1);
-            else if(stack.peek().num<hist[i])nsl.add(i-1);
-            else if(stack.peek().num>=hist[i]){
-                while(!stack.isEmpty()&&stack.peek().num>=hist[i]){
-                    stack.pop();
-                }
-                if(stack.isEmpty())nsl.add(-1);
-                else nsl.add(stack.peek().index);
+            if(stack.size()>0){
+                nsr[i]=stack.peek();
+            }else{
+                nsr[i]=heights.length;
             }
-            stack.push(new Pair(hist[i],i));
+            stack.push(i);
         }
-        int ans=Integer.MIN_VALUE;
-        int width[]=new int[hist.length];
-        for(int i=0;i<width.length;i++){
-            width[i]=(nsr.get(i)-nsl.get(i))-1;
+        return nsr;
+    }
+    
+    public int largestRectangleArea(int[] heights) {
+        int nsl[]=getnsl(heights);
+        int nsr[]=getnsr(heights);
+        int maxArea = Integer.MIN_VALUE;
+        for(int i=0;i<heights.length;i++){
+            maxArea = Math.max(maxArea,heights[i]*(nsr[i]-nsl[i]-1));
         }
-         for(int i=0;i<hist.length;i++){
-            ans=Math.max(ans,hist[i]*width[i]);
-        }
-        return ans;
+        return maxArea;
+        
     }
 }
-}
-    
-        
-    
