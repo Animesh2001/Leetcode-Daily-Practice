bb0af4988b827ecbaff60683f681058e2a768cc0
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-08-24 20:49:19 +0530

    Time: 59 ms (33.33%), Space: 47.2 MB (88.06%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.java
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -52,7 +52,7 @@ class Solution {
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]!='0'){
-                    heights[j]+=1;
+                    heights[j]+=matrix[i][j]-'0';
                 }else{
                     heights[j]=0;
                 }
