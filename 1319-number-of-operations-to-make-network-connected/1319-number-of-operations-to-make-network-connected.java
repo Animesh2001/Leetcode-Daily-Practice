fb6a8328426670feddb7a87062380f88f1fab652
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-09-04 23:58:31 +0530

    Time: 10 ms (71.10%), Space: 63.9 MB (75.39%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,5 +1,9 @@
 class Solution {
     
+//     Time Complexity :- O(E*(4alpha));
+//     Space Complexity :- O(N);
+        
+    
     public void union(int sv_parent,int ev_parent,int[]parent,int []rank)
     {
         if(rank[sv_parent]<rank[ev_parent]){
@@ -16,7 +20,6 @@ class Solution {
         if(child==parent[child]){
             return child;
         }
-        
         //path compression
         return  parent[child]= findPar(parent[child],parent);
     }
@@ -28,26 +31,24 @@ class Solution {
         for(int arr[]:connections){
             int starting_vertex = arr[0];
             int ending_vertex = arr[1];
+            //O(4alpha)
             int sv_parent = findPar(starting_vertex,parent);
             int ev_parent = findPar(ending_vertex,parent);
             
             //if parents are not same
             if(sv_parent!=ev_parent){
                 union(sv_parent,ev_parent,parent,rank);
+                //O(1)
             }else{
                 discarded++;
             }
         }
-        
         return discarded;
     }
     
     
     
     public int makeConnected(int n, int[][] connections) {
-        
-       
-       
         int parents[]=new int[n];
         for(int i=0;i<n;i++){
             parents[i]=i;
@@ -63,14 +64,8 @@ class Solution {
                 components++;
             }
         }
-        
         int required = components-1;
-        
         if(discarded<required)return -1;
-        
         return required;
-        
-        
-        
     }
 }
