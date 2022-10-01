5a09900b2337d0b8f1d6cdb62646200135a0199e
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-10-01 15:11:34 +0530

    Time: 15 ms (85.11%), Space: 45.9 MB (85.38%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,36 +1,25 @@
 class Solution {
+    
+    public int longestCommonSubsequence(String text1, String text2,int n,int m,int dp[][]){
+        if(n<0||m<0){
+            return 0;
+        }
+        if(dp[n][m]!=-1)return dp[n][m];
+        
+        if(text1.charAt(n)==text2.charAt(m)){
+            return dp[n][m]= 1+ longestCommonSubsequence(text1,text2,n-1,m-1,dp);
+        }
+        
+        return dp[n][m]= Math.max(longestCommonSubsequence(text1,text2,n-1,m,dp),longestCommonSubsequence(text1,text2,n,m-1,dp));
+    }
+    
+    
     public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length()-1;
+        int m = text2.length()-1;
         
-        int n = text1.length();
-        int m = text2.length();
-        
-        int t[][]=new int[n+1][m+1];
-        
-        //Initializing the matrix
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<m+1;j++){
-                if(i==0)t[i][j]=0;
-                if(j==0)t[i][j]=0;
-            }
-        }
-        
-        //Fill the rest of the matrix
-         for(int i=1;i<n+1;i++){
-             for(int j=1;j<m+1;j++){
-                //if last two characters of the string are same
-                if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    //call for length n-1 and m-1
-                    t[i][j]=1+t[i-1][j-1];
-                }
-                 else{
-                     t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
-                 }
-                
-             }
-        }
-        
-        return t[n][m];
-        
-        
-     }
+        int dp[][]=new int[text1.length()][text2.length()];
+        for(int rows[]:dp)Arrays.fill(rows,-1);
+        return longestCommonSubsequence(text1,text2,n,m,dp);
+    }
 }
