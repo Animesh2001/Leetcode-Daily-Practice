875da740016b3b5f5e33c58068b63c3d97319401
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-10-01 10:26:53 +0530

    Time: 11 ms (98.29%), Space: 42.2 MB (87.57%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -22,9 +22,31 @@ class Solution {
             arr[i+1]=cuts[i];
         }
         Arrays.sort(arr);
-        int dp[][]=new int[arr.length][arr.length];
-        for(int rows[]:dp)Arrays.fill(rows,-1);
-        return minCost(1,cuts.length,arr,dp);
+        int dp[][]=new int[arr.length+2][arr.length+2];
+        // for(int rows[]:dp)Arrays.fill(rows,-1);
+        // return minCost(1,cuts.length,arr,dp);
+        
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp.length;j++){
+        //         if(i>j){
+        //             dp[i][j]=0;
+        //         }
+        //     }
+        // }
+        
+        for(int i=cuts.length;i>=1;i--){
+            for(int j=1;j<=cuts.length;j++){
+                if(i>j)continue;
+                int minimum = Integer.MAX_VALUE;
+                for(int ind=i;ind<=j;ind++){
+                    int cost = arr[j+1]-arr[i-1]+dp[i][ind-1]+dp[ind+1][j];
+                    minimum = Math.min(minimum,cost);
+                }
+                dp[i][j]=minimum;
+            }
+        }
+        return dp[1][cuts.length];
+        
         
     }
 }
