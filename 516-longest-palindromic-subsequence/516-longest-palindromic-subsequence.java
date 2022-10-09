6a9e7c842317060417a7e4d6200d9c6172596a6c
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-10-09 15:11:19 +0530

    Time: 140 ms (26.79%), Space: 74.8 MB (7.77%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -1,28 +1,32 @@
 class Solution {
     
-   public String reverse_string(String s){
-       String r = "";
-       for(int i=s.length()-1;i>=0;i--){
-           r+=s.charAt(i);
-       }
-       return r;
-   }
+    public static String reverse(String str){
+        String r = "";
+        for(int i=str.length()-1;i>=0;i--){
+            r+=str.charAt(i);
+        }
+        return r;
+    }
+    
+    public static int substring(String str1,String str2,int ind1,int ind2,int dp[][]){
+        if(ind1==str1.length()||ind2==str2.length()){
+            return 0;
+        }
+        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+        if(str1.charAt(ind1)==str2.charAt(ind2)){
+            return dp[ind1][ind2]=1+substring(str1,str2,ind1+1,ind2+1,dp);
+        }
+        return dp[ind1][ind2]=Math.max(substring(str1,str2,ind1,ind2+1,dp)
+        ,substring(str1,str2,ind1+1,ind2,dp));
+        
+    }
 
     public int longestPalindromeSubseq(String s) {
-        String  r = reverse_string(s);
-        
         int n = s.length();
-        int dp[][]=new int[n+1][n+1];
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(s.charAt(i-1)==r.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+        int dp[][]=new int[n][n];
+        for(int rows[]:dp){
+            Arrays.fill(rows,-1);
         }
-        return dp[n][n];
+        return substring(s,reverse(s),0,0,dp);
     }
 }
