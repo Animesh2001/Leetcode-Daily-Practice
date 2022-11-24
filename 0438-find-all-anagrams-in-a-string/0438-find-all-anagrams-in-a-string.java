90802c4ae2ff880284c66d642ffe0eeb511459b7
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-11-24 12:38:31 +0530

    Time: 166 ms (19.18%), Space: 54.8 MB (12.82%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public boolean compare(HashMap<Character, Integer> smap, HashMap<Character, Integer> pmap) {
+        for (char sch : smap.keySet()) {
+            if (!pmap.getOrDefault(sch, 0).equals(smap.get(sch))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Integer> findAnagrams(String s, String p) {
+        HashMap<Character, Integer> smap = new HashMap<>();
+        HashMap<Character, Integer> pmap = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        
+        if(p.length()>s.length())return list;
+        
+        for (int i = 0; i < p.length(); i++) {
+            char ch = p.charAt(i);
+            pmap.put(ch, pmap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < p.length(); i++) {
+            char ch = s.charAt(i);
+            smap.put(ch, smap.getOrDefault(ch, 0) + 1);
+        }
+
+        int j = 0;
+        int i = p.length();
+
+        while (i < s.length()) {
+            if (compare(pmap, smap)) {
+                list.add(i - p.length());
+            }
+            char cha = s.charAt(i);
+            smap.put(cha, smap.getOrDefault(cha, 0) + 1);
+
+            char chr = s.charAt(i - p.length());
+            if (smap.get(chr) == 1) {
+                smap.remove(chr);
+            } else {
+                smap.put(chr, smap.get(chr) - 1);
+            }
+            i++;
+        }
+
+        if (compare(pmap, smap)) {
+            list.add(i - p.length());
+        }
+        
+        return list;
+    }
+}
