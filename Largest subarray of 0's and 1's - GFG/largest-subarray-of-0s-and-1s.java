5fedc8442267593e198b77c6cd643f7a9475b12e
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-11-28 20:45:09 +0530

    Added solution - LeetHub

---
+++ Largest subarray of 0's and 1's - GFG/largest-subarray-of-0s-and-1s.java
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.util.HashMap;
+
+class Largest_Subarray
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0)
+        {
+            int N = sc.nextInt();
+            int a[] = new int[N];
+            for (int i = 0; i < N; i++) 
+                a[i] = sc.nextInt();
+            
+
+            Solution g = new Solution();
+            int n = g.maxLen(a, a.length);
+
+            System.out.println(n);
+
+            T--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+class Solution {
+
+    // arr[] : the input array containing 0s and 1s
+    // N : size of the input array
+    
+    // return the maximum length of the subarray
+    // with equal 0s and 1s
+    int maxLen(int[] nums, int N)
+    {
+        // Your code here
+        
+         HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, -1);
+
+        int sum = 0;
+
+        int max_length = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                sum += -1;
+            } else {
+                sum += 1;
+            }
+
+            if (map.containsKey(sum)) {
+                int index = map.get(sum);
+                max_length = Math.max(max_length, i - index);
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return max_length;
+    
+        
+    }
+}
