04de18d2517e978510636bbf1dbabd4a449e7a94
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-11-29 21:42:14 +0530

    Time: 91 ms (44.94%), Space: 71.7 MB (32.86%) - LeetHub

--- 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.java
+++ 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.java
@@ -1,25 +1,26 @@
 class Solution {
+
     public boolean canReorderDoubled(int[] arr) {
-        Map<Integer,Integer>map=new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         Arrays.sort(arr);
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             int curr_val = arr[i];
-            if(map.containsKey(2*curr_val)){
-                if(map.get(2*curr_val)==1){
-                    map.remove(2*curr_val);
-                }else{
-                    map.put(2*curr_val,map.get(2*curr_val)-1);
+            if (map.containsKey(2 * curr_val)) {
+                if (map.get(2 * curr_val) == 1) {
+                    map.remove(2 * curr_val);
+                } else {
+                    map.put(2 * curr_val, map.get(2 * curr_val) - 1);
                 }
-            }else if(map.containsKey(curr_val/2)&&curr_val%2==0){
-               if(map.get(curr_val/2)==1){
-                    map.remove(curr_val/2);
-                }else{
-                    map.put(curr_val/2,map.get(curr_val/2)-1);
+            } else if (map.containsKey(curr_val / 2) && curr_val % 2 == 0) {
+                if (map.get(curr_val / 2) == 1) {
+                    map.remove(curr_val / 2);
+                } else {
+                    map.put(curr_val / 2, map.get(curr_val / 2) - 1);
                 }
-            }else{
-                map.put(curr_val,map.getOrDefault(curr_val,0)+1);
+            } else {
+                map.put(curr_val, map.getOrDefault(curr_val, 0) + 1);
             }
         }
-        return map.size()==0;
+        return map.size() == 0;
     }
 }
