ae3beb2aab22ed3de344ea26497caca7af53bca7
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-12-01 21:27:58 +0530

    Added solution - LeetHub

---
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(long arr[], int n){
+        
+        // Your code here
+        
+       int min = 0;
+       int max = n-1;
+       
+       long max_elem = arr[n-1]+1;
+       
+       for(int i=0; i<arr.length;i++){
+            if(i%2==0){
+                arr[i] = arr[i] + (arr[max]%max_elem) * max_elem;
+                max--;
+            }
+            else{
+                arr[i] = arr[i] + (arr[min]%max_elem) * max_elem;
+                min++;
+            }
+       }
+       
+       for(int i=0; i<arr.length; i++){
+           arr[i] = arr[i]/max_elem;
+       }
+        
+    }
+    
+}
+
+
