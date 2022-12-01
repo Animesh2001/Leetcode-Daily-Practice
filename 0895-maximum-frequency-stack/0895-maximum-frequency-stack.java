e22a14c94e3239f43a9a91175ea31c0ab89fb22c
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-12-01 19:22:26 +0530

    Time: 111 ms (9.26%), Space: 84.5 MB (13.97%) - LeetHub

--- 0895-maximum-frequency-stack/0895-maximum-frequency-stack.java
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.java
@@ -1,39 +1,38 @@
 class FreqStack {
-
-    Map<Integer,Stack>psm;
-    Map<Integer,Integer>fm;
+    Map<Integer, Stack> psm;
+    Map<Integer, Integer> fm;
     int max_freq = 0;
+
     public FreqStack() {
         psm = new HashMap<>();
         fm = new HashMap<>();
     }
-    
+
     public void push(int val) {
-        fm.put(val,fm.getOrDefault(val,0)+1);
-        max_freq = Math.max(max_freq,fm.get(val));
+        fm.put(val, fm.getOrDefault(val, 0) + 1);
+        max_freq = Math.max(max_freq, fm.get(val));
         int cf = fm.get(val);
-        if(!psm.containsKey(cf)){
-            Stack <Integer> stack = new Stack<>();
+        if (!psm.containsKey(cf)) {
+            Stack<Integer> stack = new Stack<>();
             stack.push(val);
-            psm.put(cf,stack);
-        }else{
+            psm.put(cf, stack);
+        } else {
             psm.get(cf).push(val);
         }
     }
-    
+
     public int pop() {
-        Stack<Integer>stack = psm.get(max_freq);
+        Stack<Integer> stack = psm.get(max_freq);
         int elem = stack.peek();
         stack.pop();
-        fm.put(elem,fm.get(elem)-1);
-        if(psm.get(max_freq).isEmpty()){
+        fm.put(elem, fm.get(elem) - 1);
+        if (psm.get(max_freq).isEmpty()) {
             psm.remove(max_freq);
             max_freq--;
         }
         return elem;
     }
 }
-
 /**
  * Your FreqStack object will be instantiated and called as such:
  * FreqStack obj = new FreqStack();
