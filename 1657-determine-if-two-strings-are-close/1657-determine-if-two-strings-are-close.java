f91f6206a5f7ffb905da913bc94848b969aba485
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-12-02 14:05:28 +0530

    Time: 108 ms (33.33%), Space: 60.2 MB (34.22%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,46 +1,25 @@
 class Solution {
 
     public boolean closeStrings(String word1, String word2) {
-        if (word1.length() != word2.length()) return false;
+        int count1[] = new int[26];
+        int count2[] = new int[26];
+        Set<Character> set1 = new HashSet<>();
+        Set<Character> set2 = new HashSet<>();
 
-        Map<Character, Integer> fm1 = new HashMap<>();
-
-        Map<Character, Integer> fm2 = new HashMap<>();
-
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < word1.length(); i++) {
-            char ch = word1.charAt(i);
-            fm1.put(ch, fm1.getOrDefault(ch, 0) + 1);
+        for (char c : word1.toCharArray()) {
+            count1[c - 'a']++;
+            set1.add(c);
         }
 
-        for (int j = 0; j < word2.length(); j++) {
-            char ch = word2.charAt(j);
-            fm2.put(ch, fm2.getOrDefault(ch, 0) + 1);
+        for (char c : word2.toCharArray()) {
+            count2[c - 'a']++;
+            set2.add(c);
         }
 
-        for (Character key : fm1.keySet()) {
-            if (!fm2.containsKey(key)) {
-                return false;
-            }
-        }
+        Arrays.sort(count1);
+        Arrays.sort(count2);
 
-        for (int val : fm2.values()) {
-            map.put(val, map.getOrDefault(val, 0) + 1);
-        }
-
-        for (int val : fm1.values()) {
-            if (map.containsKey(val)) {
-                int freq = map.get(val);
-                if (freq == 1) {
-                    map.remove(val);
-                } else {
-                    map.put(val, freq - 1);
-                }
-            }
-        }
-
-        return map.size() == 0;
+        return set1.equals(set2) && Arrays.equals(count1, count2);
     }
 }
 //word1 == word2
