ae3da1ec34c5fdb3a17bc1d7a9a7066ae034742c
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-12-03 12:05:58 +0530

    Time: 11 ms (96.95%), Space: 42.6 MB (99.23%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,37 +1,40 @@
 class Solution {
-    
-    class Pair{
+
+    class Pair {
         char ch;
         int freq;
-        
-        public Pair(char ch,int freq){
-            this.ch=ch;
-            this.freq=freq;
+
+        public Pair(char ch, int freq) {
+            this.ch = ch;
+            this.freq = freq;
         }
     }
-    
+
     public String frequencySort(String s) {
-        Map<Character,Integer>map=new HashMap<>();
-        for(char ch: s.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        Pair arr[]=new Pair[map.size()];
+        Pair arr[] = new Pair[map.size()];
         int k = 0;
-        for(char ch: map.keySet()){
+        for (char ch : map.keySet()) {
             int freq = map.get(ch);
-            Pair p = new Pair(ch,freq);
+            Pair p = new Pair(ch, freq);
             arr[k++] = p;
         }
-        Arrays.sort(arr,(a,b)->{
-            return b.freq-a.freq;
-        });
-        
+        Arrays.sort(
+            arr,
+            (a, b) -> {
+                return b.freq - a.freq;
+            }
+        );
+
         StringBuilder str = new StringBuilder();
-        
-        for(Pair p : arr){
+
+        for (Pair p : arr) {
             char ch = p.ch;
             int freq = p.freq;
-            for(int i=0;i<freq;i++){
+            for (int i = 0; i < freq; i++) {
                 str.append(ch);
             }
         }
