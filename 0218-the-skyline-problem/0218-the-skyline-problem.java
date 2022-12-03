10173736639a7953e698dd97c25fcff03f3eb02d
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-12-03 13:12:24 +0530

    Time: 460 ms (20.11%), Space: 52.2 MB (43.96%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.java
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -1,46 +1,49 @@
 class Solution {
+
     public List<List<Integer>> getSkyline(int[][] buildings) {
-        List<int[]>points = new ArrayList<>();
-        
-        for(int i=0; i<buildings.length;i++){
-            int []start = new int[]{buildings[i][0],-buildings[i][2]};
-            int []end = new int[]{buildings[i][1],buildings[i][2]};
+        List<int[]> points = new ArrayList<>();
+
+        for (int i = 0; i < buildings.length; i++) {
+            int[] start = new int[] { buildings[i][0], -buildings[i][2] };
+            int[] end = new int[] { buildings[i][1], buildings[i][2] };
             points.add(start);
             points.add(end);
         }
-        
-        Collections.sort(points,(a,b)->{
-            if(a[0]==b[0]){
-                return a[1]-b[1];
-            }else{
-                return a[0]-b[0];
+
+        Collections.sort(
+            points,
+            (a, b) -> {
+                if (a[0] == b[0]) {
+                    return a[1] - b[1];
+                } else {
+                    return a[0] - b[0];
+                }
             }
-        });
-        
-        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+        );
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         List<List<Integer>> res = new ArrayList<>();
         int ch = 0;
         pq.add(0);
-        
-        for(int i=0;i<points.size();i++){
+
+        for (int i = 0; i < points.size(); i++) {
             int nh = points.get(i)[1];
-            if(nh<0){ //starting
+            if (nh < 0) { //starting
                 pq.add(-nh);
-            }else{
+            } else {
                 pq.remove(nh);
             }
-            
-            if(ch!=pq.peek()){
-                List<Integer>temp = new ArrayList<>();
+
+            if (ch != pq.peek()) {
+                List<Integer> temp = new ArrayList<>();
                 temp.add(points.get(i)[0]);
                 temp.add(pq.peek());
-                
+
                 res.add(temp);
                 ch = pq.peek();
             }
         }
-        
+
         return res;
-        
     }
 }
