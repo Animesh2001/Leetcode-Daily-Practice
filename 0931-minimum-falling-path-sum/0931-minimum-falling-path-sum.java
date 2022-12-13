94c4e7b398a28397594527bec9865d2045ed327a
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2022-12-13 17:32:51 +0530

    Time: 3 ms (88.72%), Space: 48 MB (20.27%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,34 +1,34 @@
 class Solution {
-    
-    public int find_min(int row, int col, int matrix[][],int dp[][]){
+
+    public int find_min(int row, int col, int matrix[][], int dp[][]) {
         int n = matrix.length;
-        if( row>=n || col>=n || col<0 )return Integer.MAX_VALUE;
-        
-        if(row == n-1){
+        if (row >= n || col >= n || col < 0) return Integer.MAX_VALUE;
+
+        if (row == n - 1) {
             return matrix[row][col];
         }
-        
-        if(dp[row][col]!=-1)return dp[row][col];
-        
-        int left_diagonal =  find_min(row+1, col-1, matrix,dp);
-        int right_diagonal =  find_min(row+1, col+1, matrix,dp);
-        int down =  find_min(row+1, col, matrix,dp);
-        
-        return dp[row][col] = matrix[row][col] + Math.min(left_diagonal , Math.min(right_diagonal,down));
+
+        if (dp[row][col] != -1) return dp[row][col];
+
+        int left_diagonal = find_min(row + 1, col - 1, matrix, dp);
+        int right_diagonal = find_min(row + 1, col + 1, matrix, dp);
+        int down = find_min(row + 1, col, matrix, dp);
+
+        return dp[row][col] = matrix[row][col] + Math.min(left_diagonal, Math.min(right_diagonal, down));
     }
-    
+
     public int minFallingPathSum(int[][] matrix) {
         int ans = Integer.MAX_VALUE;
         int dp[][] = new int[matrix.length][matrix.length];
-        
-        for(int rows[] : dp){
-            Arrays.fill(rows,-1);
+
+        for (int rows[] : dp) {
+            Arrays.fill(rows, -1);
         }
-        
-        for(int i=0; i<matrix[0].length; i++){
-            ans = Math.min(ans,find_min(0, i, matrix,dp));
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            ans = Math.min(ans, find_min(0, i, matrix, dp));
         }
-        
+
         return ans;
     }
 }
