012625a6853e676e8455eb79d9290b669b7447a9
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2023-01-01 13:30:28 +0530

    Time: 1 ms (95.36%), Space: 40.4 MB (74.62%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,28 +1,27 @@
 class Solution {
+
     public boolean wordPattern(String pattern, String s) {
-        HashMap<Character,String>map=new HashMap<>();
-        HashMap<String,Boolean>tn = new HashMap<>();
-        
-        String str[]=s.split(" ");
-        
-        if(pattern.length()!=str.length)return false;
-        
-        for(int i=0;i<pattern.length();i++){
+        HashMap<Character, String> map = new HashMap<>();
+        HashMap<String, Integer> check = new HashMap<>();
+        String[] arr = s.split(" ");
+
+        if (arr.length != pattern.length()) return false;
+
+        for (int i = 0; i < pattern.length(); i++) {
             char ch = pattern.charAt(i);
-            if(!map.containsKey(ch)){
-                if(tn.containsKey(str[i])){
+            if (map.containsKey(ch)) {
+                if (!map.get(ch).equals(arr[i])) {
                     return false;
                 }
-                map.put(ch,str[i]);
-                tn.put(str[i],true);
-            }else{
-                String inmap = map.get(ch);
-                if(!inmap.equals(str[i])){
+            } else {
+                if (check.containsKey(arr[i])) {
                     return false;
                 }
+                map.put(ch, arr[i]);
+                check.put(arr[i], 1);
             }
         }
-        
+
         return true;
     }
 }
