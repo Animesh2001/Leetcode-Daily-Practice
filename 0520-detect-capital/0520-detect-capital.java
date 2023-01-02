d1397c8407e225b3a9e1dce98711656d4bbd49db
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2023-01-02 14:16:29 +0530

    Time: 1 ms (100.00%), Space: 40.7 MB (87.47%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,37 +1,11 @@
 class Solution {
 
     public boolean detectCapitalUse(String word) {
-        char arr[] = word.toCharArray();
-        boolean ans = true;
-
-        //if first character is capital
-        if (arr[0] >= 65 && arr[0] <= 90) {
-            for (int i = 1; i < arr.length; i++) {
-                if (arr[i] >= 97 && arr[i] <= 122) {
-                    //if remaining characters are capital
-                    ans = false;
-                    break;
-                }
-            }
-            if (ans == false) {
-                ans = true;
-                for (int i = 1; i < arr.length; i++) {
-                    if (arr[i] >= 65 && arr[i] <= 90) {
-                        //if remaining characters are small
-                        ans = false;
-                        break;
-                    }
-                }
-            }
-        } else {
-            for (int i = 1; i < arr.length; i++) {
-                if (arr[i] >= 65 && arr[i] <= 90) {
-                    ans = false;
-                    break;
-                }
-            }
+        int capitals = 0;
+        for (char ch : word.toCharArray()) {
+            if (Character.isUpperCase(ch)) capitals++;
         }
-
-        return ans;
+        if (capitals == word.length() || capitals == 0) return true;
+        return capitals == 1 && Character.isUpperCase(word.charAt(0));
     }
 }
