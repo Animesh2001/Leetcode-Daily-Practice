8e7cf32f377f3cba2811c60b0ec9f0896fbf99c5
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2023-01-22 13:49:00 +0530

    Time: 20 ms (37.33%), Space: 186.4 MB (20.39%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,30 @@
+class Solution {
+        public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = 0; j <= i; j++) {
+                if(s.charAt(i) == s.charAt(j) && (i - j <= 2 || dp[j+1][i-1])) {
+                    dp[j][i] = true;
+                }
+            }
+        }
+        helper(res, new ArrayList<>(), dp, s, 0);
+        return res;
+    }
+    
+    private void helper(List<List<String>> res, List<String> path, boolean[][] dp, String s, int pos) {
+        if(pos == s.length()) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for(int i = pos; i < s.length(); i++) {
+            if(dp[pos][i]) {
+                path.add(s.substring(pos,i+1));
+                helper(res, path, dp, s, i+1);
+                path.remove(path.size()-1);
+            }
+        }
+    }
+}
