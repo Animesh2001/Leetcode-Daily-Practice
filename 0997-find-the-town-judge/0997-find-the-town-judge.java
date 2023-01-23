c429a7a9836f992fe66643c4656dc5c7850062c7
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2023-01-23 10:44:10 +0530

    Time: 30 ms (11.75%), Space: 96.3 MB (5.79%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,19 +1,36 @@
 class Solution {
 
-   public int findJudge(int n, int[][] trust) {
-        if (n == 1) return 1;
-        Map<Integer, Integer> normalPeople = new HashMap<>();
-        for(int i = 1; i <= n; i++) {
-            normalPeople.put(i, 0);
+    public int findJudge(int n, int[][] trust) {
+        int[][] mat = new int[n + 1][n + 1];
+
+        for (int i = 0; i < trust.length; i++) {
+            mat[trust[i][0]][trust[i][1]] = 1;
         }
-        Map<Integer, Integer> candidate = new HashMap<>();
-        for (int[] a : trust) {
-            normalPeople.put(a[0], normalPeople.get(a[0]) + 1);
-            candidate.put(a[1], candidate.getOrDefault(a[1], 0) + 1);
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 1; i <= n; i++) stack.push(i);
+
+        while (stack.size() > 1) {
+            int first = stack.pop();
+            int second = stack.pop();
+
+            if (mat[first][second] == 1) {
+                stack.push(second);
+            } else {
+                stack.push(first);
+            }
         }
-        for (int i : candidate.keySet()) {
-            if(candidate.get(i) == n - 1 && normalPeople.get(i) == 0) return i; 
+
+        for (int i = 1; i < n + 1; i++) {
+            if (mat[stack.peek()][i] == 1) {
+                return -1;
+            }
+            if (i != stack.peek() && mat[i][stack.peek()] == 0) {
+                return -1;
+            }
         }
-        return -1;
+
+        return stack.peek();
     }
 }
