607135135b43696f8d6b0293f0991b636957e55b
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2023-01-29 16:25:04 +0530

    Time: 1 ms (91.95%), Space: 39.8 MB (95.34%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
@@ -1,55 +1,40 @@
 class Solution {
-    public int max = 0;
 
     public int maxScoreWords(String[] words, char[] letters, int[] score) {
-        HashMap<Character, Integer> letter = new HashMap<>();
+        int freq[] = new int[26];
         for (int i = 0; i < letters.length; i++) {
-            letter.put(letters[i], letter.getOrDefault(letters[i], 0) + 1);
+            freq[letters[i] - 'a']++;
         }
-        ArrayList<String> list = new ArrayList<>();
-        solve(0, words, list, letter, score);
-        return max;
+        return solve(words, freq, score, 0);
     }
 
-    public boolean isstrvalid(String str, HashMap<Character, Integer> letter, HashMap<Character, Integer> container) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char ch : str.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-            if (!letter.containsKey(ch)) return false;
-            if (map.get(ch) > letter.get(ch)) return false;
-        }
-        for (char ch : map.keySet()) {
-            if (container.containsKey(ch) && map.get(ch) + container.get(ch) > letter.get(ch)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public void solve(int i, String[] words, ArrayList<String> list, HashMap<Character, Integer> letter, int[] score) {
-        if (i == words.length) {
-            HashMap<Character, Integer> map = new HashMap<>();
-            for (String str : list) {
-                if (isstrvalid(str, letter, map)) {
-                    for (char ch : str.toCharArray()) {
-                        map.put(ch, map.getOrDefault(ch, 0) + 1);
-                        if (map.get(ch) > letter.get(ch)) {
-                            return;
-                        }
-                    }
-                }
-            }
-            int currscore = 0;
-            for (char ch : map.keySet()) {
-                currscore += (score[ch - 'a'] * map.get(ch));
-            }
-            max = Math.max(max, currscore);
-            return;
+    public int solve(String words[], int[] freq, int[] score, int idx) {
+        if (idx == words.length) {
+            return 0;
         }
 
-        solve(i + 1, words, list, letter, score);
-        list.add(words[i]);
-        solve(i + 1, words, list, letter, score);
-        list.remove(list.size() - 1);
+        int n = 0 + solve(words, freq, score, idx + 1); //No
+
+        int sword = 0;
+        boolean flag = true;
+        String word = words[idx];
+        for (int i = 0; i < word.length(); i++) {
+            if (freq[word.charAt(i) - 'a'] == 0) {
+                flag = false;
+            }
+            freq[word.charAt(i) - 'a']--;
+            sword += score[word.charAt(i) - 'a'];
+        }
+
+        int s = 0;
+        if (flag) {
+            s = sword + solve(words, freq, score, idx + 1);
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            freq[word.charAt(i) - 'a']++;
+        }
+
+        return Math.max(n, s);
     }
 }
