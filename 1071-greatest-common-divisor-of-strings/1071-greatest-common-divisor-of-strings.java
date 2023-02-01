1ccf945ea2b13e9b2b73a11ef44037e7e273c079
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2023-02-02 00:51:07 +0530

    Time: 1 ms (85.83%), Space: 42.3 MB (55.91%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,32 +1,16 @@
 class Solution {
 
-    public int lcm(int divisor, int dividend) {
-        if (dividend % divisor == 0) {
-            return divisor;
-        }
-
-        return lcm(dividend % divisor, divisor);
+    public int gcd(int divisor, int dividend) {
+        return divisor == 0 ? dividend : gcd(dividend % divisor, divisor);
     }
 
     public String gcdOfStrings(String str1, String str2) {
-        int los1 = str1.length();
-        int los2 = str2.length();
-        int cd = lcm(los1, los2);
-
-        String ds = str1.substring(0, cd);
-        boolean flag = true;
-        for (int i = 0; i < str1.length(); i += cd) {
-            if (!str1.substring(i, i + cd).equals(ds)) {
-                return "";
-            }
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
         }
 
-        for (int i = 0; i < str2.length(); i += cd) {
-            if (!str2.substring(i, i + cd).equals(ds)) {
-                return "";
-            }
-        }
+        int gcd = gcd(str1.length(), str2.length());
 
-        return ds;
+        return str1.substring(0, gcd);
     }
 }
