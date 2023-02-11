5b07c047f681403e318959f7f3d3a19d58998c55
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2023-02-11 19:53:02 +0530

    Time: 3 ms (64.44%), Space: 43.2 MB (37.41%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<List<Integer>>ans = new ArrayList<>();
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        List<Integer>list = new ArrayList<>();
+        permuteUnique(map,1,nums.length,list);
+        return ans;
+    }
+    
+    public void permuteUnique(HashMap<Integer,Integer>map, int cs, int ts, List<Integer>list){
+        
+        if(cs>ts){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        
+        
+        for(int num : map.keySet()){
+            if(map.get(num)>0){
+                map.put(num,map.get(num)-1);
+                list.add(num);
+                permuteUnique(map,cs+1,ts,list);
+                list.remove(list.size()-1);
+                map.put(num,map.get(num)+1);
+            }
+        }
+    }
+}
