edf5f785e01c27a6920677f1cae0682a2c0163ab
Animesh2001 <71101768+Animesh2001@users.noreply.github.com>
2023-05-08 23:26:57 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (54.00%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -23,7 +23,7 @@ public class Solution {
         ListNode slow = head;
         ListNode fast = head;
         
-        while(fast!=null && fast.next!=null){
+        while(fast.next!=null && fast.next.next!=null){
             slow = slow.next;
             fast = fast.next.next;
             
